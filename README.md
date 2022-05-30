9005b99c98ef784b281b6b4054989fbfd00698b7
kkocdko <kkocdko@163.com>
2022-05-30 22:05:48 +0800

    Bump version to `0.2.4`

--- README.md
+++ README.md
@@ -8,14 +8,8 @@ Void file generator, supports many formats.
 
 - `VoidMTS`: To be implemented.
 
-- `VoidBMP`: To be implemented.
-
 - `VoidPNG`: To be implemented.
 
 - `VoidJPEG`: To be implemented.
 
-- Error handling for illegal config.
-
 - Example server using [tiny-http](https://github.com/tiny-http/tiny-http).
-
-- Performance improvements with `unsafe`.
--- examples/server/src/main.rs
+++ examples/server/src/main.rs
@@ -8,10 +8,9 @@ fn respond(req: Request, data: &Vec<u8>, mime: &str) {
 }
 
 fn main() {
-    let void_pdf = VoidPDF {
+    let void_bmp = VoidBMP {
         width: 10,
         height: 10,
-        page_count: 1,
     }
     .data();
     let void_flv = VoidFLV {
@@ -19,23 +18,30 @@ fn main() {
         fps: 2.0,
     }
     .data();
+    let void_m3u = VoidM3U {
+        duration: 3600.0,
+        filename: "void.wav".to_string(),
+    }
+    .data();
+    let void_pdf = VoidPDF {
+        width: 10,
+        height: 10,
+        page_count: 1,
+    }
+    .data();
     let void_wav = VoidWAV {
         duration: 3600.0,
         ..Default::default()
     }
     .data();
-    let void_m3u8 = VoidM3U {
-        duration: 3600.0,
-        filename: "void.wav".to_string(),
-    }
-    .data();
-    println!("[fvoid server] address = 0.0.0.0:9902");
+    println!("address = 0.0.0.0:9902");
     for req in Server::http("0.0.0.0:9902").unwrap().incoming_requests() {
         match req.url() {
+            "/void.bmp" => respond(req, &void_bmp, "image/bmp"),
             "/void.pdf" => respond(req, &void_pdf, "application/pdf"),
             "/void.flv" => respond(req, &void_flv, "video/x-flv"),
             "/void.wav" => respond(req, &void_wav, "audio/wav"),
-            "/void.m3u8" => respond(req, &void_m3u8, "application/mpegurl"),
+            "/void.m3u" | "/void.m3u8" => respond(req, &void_m3u, "application/mpegurl"),
             _ => {
                 req.respond(Response::empty(404)).unwrap();
             }
